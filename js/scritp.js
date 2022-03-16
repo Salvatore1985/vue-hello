b87a14785487ff5e757e1b00f7cc04ccc9b4435c
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-03-16 14:59:54 +0100

    problemi nell' inserimento vue nel dom

---
+++ js/scritp.js
@@ -0,0 +1,15 @@
+/**
+ * Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data, magari volendo sperimentare anche prendendola da un input.
+ */
+console.log("Prova ", Vue)
+const root = new Vue(
+    {
+        el: "#root",
+        data: {
+            message: "Hello Word",
+        },
+    }
+);
